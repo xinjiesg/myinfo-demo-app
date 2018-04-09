86166a3dd2f30d2b2d162d5568a68f3ad4a0a1dc
Jason Zheng <jasonzjx@gmail.com>
2018-04-09 16:44:07 +0800

    Updated for new tenant JWS

--- routes/index.js
+++ routes/index.js
@@ -27,226 +27,266 @@ var _clientId = process.env.MYINFO_APP_CLIENT_ID;
 // LOADED FRON ENV VARIABLE: your client_secret provided to you during onboarding
 var _clientSecret = process.env.MYINFO_APP_CLIENT_SECRET;
 // redirect URL for your web application
-var _redirectUrl =  process.env.MYINFO_APP_REDIRECT_URL;
+var _redirectUrl = process.env.MYINFO_APP_REDIRECT_URL;
 // default realm for your web application
 var _realm = process.env.MYINFO_APP_REALM;
 
 // URLs for MyInfo APIs
 var _authLevel = process.env.AUTH_LEVEL;
 
-var _authApiUrl =  process.env.MYINFO_API_AUTHORISE;
+var _authApiUrl = process.env.MYINFO_API_AUTHORISE;
 var _tokenApiUrl = process.env.MYINFO_API_TOKEN;
 var _personApiUrl = process.env.MYINFO_API_PERSON;
 
-var _attributes = "name,sex,race,nationality,dob,email,mobileno,regadd,housingtype,hdbtype,marital,edulevel,assessableincome,hanyupinyinname,aliasname,hanyupinyinaliasname,marriedname,cpfcontributions,cpfbalances";
+var _attributes = "name,sex,race,nationality,dob,email,mobileno,regadd,housingtype,hdbtype,marital,edulevel,assessableincome";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/../views/html/index.html'));
+  res.sendFile(path.join(__dirname + '/../views/html/index.html'));
 });
 
 // callback function - directs back to home page
 router.get('/callback', function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/../views/html/index.html'));
+  res.sendFile(path.join(__dirname + '/../views/html/index.html'));
 });
 
 // function for getting environment variables to the frontend
 router.get('/getEnv', function(req, res, next) {
-	if (_clientId == undefined || _clientId == null)
-		res.jsonp({status: "ERROR", msg:"client_id not found"});
-	else
-		res.jsonp({status: "OK", clientId: _clientId, redirectUrl: _redirectUrl, authApiUrl: _authApiUrl, attributes: _attributes, authLevel: _authLevel});
+  if (_clientId == undefined || _clientId == null)
+    res.jsonp({
+      status: "ERROR",
+      msg: "client_id not found"
+    });
+  else
+    res.jsonp({
+      status: "OK",
+      clientId: _clientId,
+      redirectUrl: _redirectUrl,
+      authApiUrl: _authApiUrl,
+      attributes: _attributes,
+      authLevel: _authLevel
+    });
 });
 
 // function for frontend to call backend
 router.post('/getPersonData', function(req, res, next) {
-	// get variables from frontend
-	var code = req.body.code;
+  // get variables from frontend
+  var code = req.body.code;
 
-	var data;
-    var request;
-    
-    // **** CALL TOKEN API ****
-	request = createTokenRequest(code);
-    request
-        .buffer(true)
-        .end(function (callErr, callRes) {
-            if (callErr) {
-                // ERROR
-                res.jsonp({status: "ERROR", msg: callErr});
-            } else {
-                // SUCCESSFUL
-                var data = {body: callRes.body, text: callRes.text};
-                console.log("Response from Token API:");
-                console.log(JSON.stringify(data.body));
+  var data;
+  var request;
 
-                var accessToken = data.body.access_token;
-                if (accessToken == undefined || accessToken == null) {
-                    res.jsonp({status: "ERROR", msg: "ACCESS TOKEN NOT FOUND"});
-                }
-
-                // everything ok, call person API
-                callPersonAPI(accessToken, res);
-            }
+  // **** CALL TOKEN API ****
+  request = createTokenRequest(code);
+  request
+    .buffer(true)
+    .end(function(callErr, callRes) {
+      if (callErr) {
+        // ERROR
+        res.jsonp({
+          status: "ERROR",
+          msg: callErr
         });
+      } else {
+        // SUCCESSFUL
+        var data = {
+          body: callRes.body,
+          text: callRes.text
+        };
+        console.log("\x1b[32m", "Response from Token API:", "\x1b[0m");
+        console.log(JSON.stringify(data.body));
+
+        var accessToken = data.body.access_token;
+        if (accessToken == undefined || accessToken == null) {
+          res.jsonp({
+            status: "ERROR",
+            msg: "ACCESS TOKEN NOT FOUND"
+          });
+        }
+
+        // everything ok, call person API
+        callPersonAPI(accessToken, res);
+      }
+    });
 });
- 
-function callPersonAPI (accessToken, res) {
-	// validate and decode token to get UINFIN
-	var decoded = securityHelper.verifyJWS(accessToken, _publicCertContent);
-	if (decoded == undefined || decoded == null) {
-		res.jsonp({status: "ERROR", msg: "INVALID TOKEN"})
-	}
 
-	console.log("Decoded Access Token:");
-	console.log(JSON.stringify(decoded));
+function callPersonAPI(accessToken, res) {
+  // validate and decode token to get UINFIN
+  var decoded = securityHelper.verifyJWS(accessToken, _publicCertContent);
+  if (decoded == undefined || decoded == null) {
+    res.jsonp({
+      status: "ERROR",
+      msg: "INVALID TOKEN"
+    })
+  }
 
-	var uinfin = decoded.sub;
-    if (uinfin == undefined || uinfin == null) {
-	    res.jsonp({status: "ERROR", msg: "UINFIN NOT FOUND"});
-    } 
-	
-    // **** CALL PERSON API ****
-	var request = createPersonRequest (uinfin, accessToken);
-	
-	// Invoke asynchronous call
-    request
-        .buffer(true)
-        .end(function (callErr, callRes) {
-            if (callErr) {
-                res.jsonp({status: "ERROR", msg: callErr});
-            } else {
-                // SUCCESSFUL
-                var data = {body: callRes.body, text: callRes.text};
-                console.log("Response from Person API:");
-                console.log(JSON.stringify(data.text));
-                var personJWS = data.text;
-                if (personJWS == undefined || personJWS == null) {
-                    res.jsonp({status: "ERROR", msg: "PERSON DATA NOT FOUND"});
-                } else {
-                    console.log("Person Data (JWS): "+ personJWS);
+  console.log("\x1b[32m", "Decoded Access Token:", "\x1b[0m");
+  console.log(JSON.stringify(decoded));
 
-                    var personData = personJWS;
-                    // verify signature & decode JWS to get the JSON
-                    personData = securityHelper.verifyJWS(personJWS, _publicCertContent);
-                    if (personData == undefined || personData == null)
-                        res.jsonp({status: "ERROR", msg: "INVALID DATA OR SIGNATURE FOR PERSON DATA"});
-                    personData.uinfin = uinfin; // add the uinfin into the data to display on screen
+  var uinfin = decoded.sub;
+  if (uinfin == undefined || uinfin == null) {
+    res.jsonp({
+      status: "ERROR",
+      msg: "UINFIN NOT FOUND"
+    });
+  }
 
-                    console.log("Person Data (Decoded): "+JSON.stringify(personData));
-                    // successful. return data back to frontend
-                    res.jsonp({status: "OK", text: personData});
-                }
-            }
+  // **** CALL PERSON API ****
+  var request = createPersonRequest(uinfin, accessToken);
+
+  // Invoke asynchronous call
+  request
+    .buffer(true)
+    .end(function(callErr, callRes) {
+      if (callErr) {
+        res.jsonp({
+          status: "ERROR",
+          msg: callErr
         });
+      } else {
+        // SUCCESSFUL
+        var data = {
+          body: callRes.body,
+          text: callRes.text
+        };
+        var personJWS = data.text;
+        if (personJWS == undefined || personJWS == null) {
+          res.jsonp({
+            status: "ERROR",
+            msg: "PERSON DATA NOT FOUND"
+          });
+        } else {
+          console.log("\x1b[32m", "Person Data (JWS):", "\x1b[0m");
+          console.log(personJWS);
+
+          var personData = personJWS;
+          // verify signature & decode JWS to get the JSON
+          personData = securityHelper.verifyJWS(personJWS, _publicCertContent);
+          if (personData == undefined || personData == null)
+            res.jsonp({
+              status: "ERROR",
+              msg: "INVALID DATA OR SIGNATURE FOR PERSON DATA"
+            });
+          personData.uinfin = uinfin; // add the uinfin into the data to display on screen
+
+          console.log("\x1b[32m", "Person Data (Decoded):", "\x1b[0m");
+          console.log(JSON.stringify(personData));
+          // successful. return data back to frontend
+          res.jsonp({
+            status: "OK",
+            text: personData
+          });
+        }
+      }
+    });
 }
 
 // function to prepare request for TOKEN API
 function createTokenRequest(code) {
-    var cacheCtl = "no-cache";
-    var contentType = "application/x-www-form-urlencoded";
-    var method = "POST";
+  var cacheCtl = "no-cache";
+  var contentType = "application/x-www-form-urlencoded";
+  var method = "POST";
 
-    // assemble params for Token API
-    var strParams = "grant_type=authorization_code" 
-				+ "&code=" + code 
-				+ "&redirect_uri=" + _redirectUrl
-				+ "&client_id=" + _clientId
-				+ "&client_secret=" + _clientSecret
-				;
-    var params = querystring.parse(strParams);
+  // assemble params for Token API
+  var strParams = "grant_type=authorization_code" +
+    "&code=" + code +
+    "&redirect_uri=" + _redirectUrl +
+    "&client_id=" + _clientId +
+    "&client_secret=" + _clientSecret;
+  var params = querystring.parse(strParams);
 
-    
-    // assemble headers for Token API
-    var strHeaders = "Content-Type=" + contentType + "&Cache-Control=" + cacheCtl;
-    var headers = querystring.parse(strHeaders);
-    
-    // Add Authorisation headers for connecting to API Gateway
-    var authHeaders = null;
-    if (_authLevel == "L0") {
-        // No headers
-    } else if (_authLevel == "L2") {
-        authHeaders = securityHelper.generateAuthorizationHeader(
-            _tokenApiUrl,
-            params,
-            method,
-            contentType,
-            _authLevel,
-            _clientId,
-            _privateKeyContent,
-            _clientSecret,
-            _realm
-        );
-    } else {
-        throw new Error("Unknown Auth Level");
-    }
 
-    if(!_.isEmpty(authHeaders)) {
-        _.set(headers, "Authorization", authHeaders);
-    }
+  // assemble headers for Token API
+  var strHeaders = "Content-Type=" + contentType + "&Cache-Control=" + cacheCtl;
+  var headers = querystring.parse(strHeaders);
 
-    console.log("Request Header for Token API: "+JSON.stringify(headers));
+  // Add Authorisation headers for connecting to API Gateway
+  var authHeaders = null;
+  if (_authLevel == "L0") {
+    // No headers
+  } else if (_authLevel == "L2") {
+    authHeaders = securityHelper.generateAuthorizationHeader(
+      _tokenApiUrl,
+      params,
+      method,
+      contentType,
+      _authLevel,
+      _clientId,
+      _privateKeyContent,
+      _clientSecret,
+      _realm
+    );
+  } else {
+    throw new Error("Unknown Auth Level");
+  }
 
-    var request = restClient.post(_tokenApiUrl);
+  if (!_.isEmpty(authHeaders)) {
+    _.set(headers, "Authorization", authHeaders);
+  }
 
-    // Set headers
-    if(!_.isUndefined(headers) && !_.isEmpty(headers)) 
-    	request.set(headers);
+  console.log("\x1b[32m", "Request Header for Token API:", "\x1b[0m");
+  console.log(JSON.stringify(headers));
 
-    // Set Params
-    if(!_.isUndefined(params) && !_.isEmpty(params))
-        request.send(params);
+  var request = restClient.post(_tokenApiUrl);
 
-	return request;
+  // Set headers
+  if (!_.isUndefined(headers) && !_.isEmpty(headers))
+    request.set(headers);
+
+  // Set Params
+  if (!_.isUndefined(params) && !_.isEmpty(params))
+    request.send(params);
+
+  return request;
 }
 
 // function to prepare request for PERSON API
-function createPersonRequest (uinfin, validToken) {
-    var url = _personApiUrl+ "/" + uinfin + "/";
-    var cacheCtl = "no-cache";
-    var method = "GET";
+function createPersonRequest(uinfin, validToken) {
+  var url = _personApiUrl + "/" + uinfin + "/";
+  var cacheCtl = "no-cache";
+  var method = "GET";
 
-    // assemble params for Person API
-    var strParams = "client_id=" + _clientId
-				+ "&attributes=" + _attributes;
-    var params = querystring.parse(strParams);
+  // assemble params for Person API
+  var strParams = "client_id=" + _clientId +
+    "&attributes=" + _attributes;
+  var params = querystring.parse(strParams);
 
-    // assemble headers for Person API
-    var strHeaders = "Cache-Control=" + cacheCtl ; 
-    var headers = querystring.parse(strHeaders);
-    
-    // Add Authorisation headers for connecting to API Gateway
-    var authHeaders = securityHelper.generateAuthorizationHeader(
-    	url, 
-    	params, 
-    	method, 
-    	"", // no content type needed for GET 
-    	_authLevel,
-    	_clientId,
-    	_privateKeyContent,
-    	_clientSecret,
-    	_realm
-    	);
+  // assemble headers for Person API
+  var strHeaders = "Cache-Control=" + cacheCtl;
+  var headers = querystring.parse(strHeaders);
 
-    // NOTE: include access token in Authorization header as "Bearer " (with space behind)
-    if(!_.isEmpty(authHeaders)) {
-        _.set(headers, "Authorization", authHeaders + ",Bearer " + validToken);
-    } else {
-        _.set(headers, "Authorization", "Bearer " + validToken);
-    }
+  // Add Authorisation headers for connecting to API Gateway
+  var authHeaders = securityHelper.generateAuthorizationHeader(
+    url,
+    params,
+    method,
+    "", // no content type needed for GET
+    _authLevel,
+    _clientId,
+    _privateKeyContent,
+    _clientSecret,
+    _realm
+  );
 
-	// invoke token API
-    var request = restClient.get(url);
+  // NOTE: include access token in Authorization header as "Bearer " (with space behind)
+  if (!_.isEmpty(authHeaders)) {
+    _.set(headers, "Authorization", authHeaders + ",Bearer " + validToken);
+  } else {
+    _.set(headers, "Authorization", "Bearer " + validToken);
+  }
 
-    // Set headers
-    if(!_.isUndefined(headers) && !_.isEmpty(headers)) 
-    	request.set(headers);
+  // invoke token API
+  var request = restClient.get(url);
 
-    // Set Params
-    if(!_.isUndefined(params) && !_.isEmpty(params))
-        request.query(params);
+  // Set headers
+  if (!_.isUndefined(headers) && !_.isEmpty(headers))
+    request.set(headers);
 
-	return request;
+  // Set Params
+  if (!_.isUndefined(params) && !_.isEmpty(params))
+    request.query(params);
+
+  return request;
 }
 
 module.exports = router;
